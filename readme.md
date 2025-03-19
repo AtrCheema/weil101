2b60d46785f67b9fc00c2675664cddb91f473f11
AtrCheema <ather_abbas786@yahoo.com>
2025-03-19 15:21:38 +0000

    removed commens in eda

--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
 
-Code for the paper [Machine learning analysis to interpret the effect of the photocatalytic reaction rate constant (k) of semiconductor-based photocatalysts on dye removal]( https://doi.org/10.1016/j.jhazmat.2023.132995) .
+Code for the paper [Machine learning analysis to interpret the effect of the photocatalytic reaction rate constant (k) of semiconductor-based photocatalysts on dye removal]( https://doi.org/10.1016/j.jhazmat.2023.132995) in Journal of Hazardous Materials.
 
-Online reprducible examples running on readthedocs are at https://weil101.readthedocs.io/en/latest/ .
+# AI for Photocatalysis
+In this study we performed data-driven modeling of photocatalysis process. The objective was to build a machine learning (ML) model to predict first order rate constant k using the experimental conditions (Time, solution pH, Light intensity, Light source distance, dye concentration loading), elemental composition of catalyst (C, Fe, Al, Ni, Mo, S, Bi, Ag, Pd, Pt) physio-chemical properties of the catalyst (Volume, surface area, pore size, pore volume) and parameters of pollutant (solubility, molecular weight, H-bond acceptor and donor counts). Total data consisted of 1527 samples and 32 features, which were collected by experimentation. This dataset was divided into 1068 (70%) training set and 459 (30%) test set. In the first notebook [Exploratory Data Analysis](https://weil101.readthedocs.io/en/latest/auto_examples/eda.htmly) we performed exploratory data analysis. After this we checked the performance of avaialble (over 30) machine learning algorithms on test set of our data in [Experiments](https://weil101.readthedocs.io/en/latest/auto_examples/experiments.html) after training them on training set. The purpose was to get an idea that which ML algorithm will be best for our problem. After that, we performed feature selection using various feature selection methods in [Feature Selection notebook](https://weil101.readthedocs.io/en/latest/auto_examples/brouta_feature_selection.html). The final features were selected using [Boruta-shap](https://doi.org/10.18637/jss.v036.i11) method. After selecting the algorithm and features, we performed hyperparameter optimization using k-fold cross validation in [hyperparameter optimization](https://weil101.readthedocs.io/en/latest/auto_examples/hpo.html). Then we built and trained our model on training set and checked its prediction performance on test set. Some plots depicting analysis of prediction performance and error anlaysis were also plotted here. After that we interpreted the machine learning model using various post-hoc interpretation methods. This includes [SHAP](https://arxiv.org/abs/1705.07874), [Partial Dependence Plots](https://hastie.su.domains/ElemStatLearn/) and [Accumulated Local Effects](https://doi.org/10.1111/rssb.12377). Finally we checked the robustness of our model by quantifying uncertainty in the prediction of machine learning model. We used [conformal analysis](https://jmlr.csail.mit.edu/papers/volume9/shafer08a/shafer08a.pdf) for this purpose and analyzed the robustness of our model by employing various conformal anlaysis methods.
 
-In order to reproduce the results, follow the following steps
+# Reproducibility
+The results presented in these notebooks are completely (~100%) reproducible. All you need is to use same computational environment which was used to create these results. The names and versions of the python packages used in this project are given in requirements.txt file. Furthermore, the exact version of some of the python packages is also printed at the start of each notebook. The user will have to install these packages, preferably in a new conda environment. Then make sure that you have copied all the code in utils notebook in a utils.py file and saved in the same direcotry/folder where other python scripts are present. The data file is expected to be in the data folder. These steps can be summarized as below
 
     git clone https://github.com/AtrCheema/weil101.git
 
@@ -12,3 +14,7 @@ In order to reproduce the results, follow the following steps
     pip install -r docs/requirements.txt
 
     make html
+
+
+Online reprducible examples running on readthedocs are at https://weil101.readthedocs.io/en/latest/ .
+
--- scripts/eda.py
+++ scripts/eda.py
@@ -48,231 +48,229 @@ data = read_data(outputs=['k', 'Efficiency'])
 
 print(data.shape)
 
-# # %%
-# # printing counts of missing values
-# data.isna().sum()
+# %%
+# printing counts of missing values
+data.isna().sum()
 
-# # %%
+# %%
 
-# data.describe()
+data.describe()
 
-# # %%
-# print(len(data['Catalyst'].unique()))
+# %%
+print(len(data['Catalyst'].unique()))
 
-# data['Catalyst'].unique()
+data['Catalyst'].unique()
 
-# # %%
-# colors, _ = map_array_to_cmap(np.arange(len(data['Catalyst'].unique())), cmap="tab20")
-# pie(data['Catalyst'], colors=colors)
+# %%
+colors, _ = map_array_to_cmap(np.arange(len(data['Catalyst'].unique())), cmap="tab20")
+pie(data['Catalyst'], colors=colors)
 
-# # %%
-# # Anions are representative of inorganic content in the xyz.
+# %%
+# Anions are representative of inorganic content in the xyz.
 
-# print(len(data['Anions'].unique()))
+print(len(data['Anions'].unique()))
 
-# data['Anions'].unique()
+data['Anions'].unique()
 
-# # %%
-# colors, _ = map_array_to_cmap(np.arange(len(data['Anions'].unique())), cmap="tab20")
-# pie(data['Anions'], colors=colors)
+# %%
+colors, _ = map_array_to_cmap(np.arange(len(data['Anions'].unique())), cmap="tab20")
+pie(data['Anions'], colors=colors)
 
-# # %%
+# %%
 
-# print(len(data['Dye'].unique()))
+print(len(data['Dye'].unique()))
 
-# data['Dye'].unique()
+data['Dye'].unique()
 
-# # %%
-# colors, _ = map_array_to_cmap(np.arange(len(data['Dye'].unique())), cmap="tab20")
-# pie(data['Dye'], colors=colors)
+# %%
+colors, _ = map_array_to_cmap(np.arange(len(data['Dye'].unique())), cmap="tab20")
+pie(data['Dye'], colors=colors)
 
-# # %%
-# # Overall distribution of all features
+# %%
+# Overall distribution of all features
 
-# data_num = data.drop(columns=['Catalyst', 'Anions', 'Dye'])
+data_num = data.drop(columns=['Catalyst', 'Anions', 'Dye'])
 
 # rearrange_columns in data_num so that same categories lie close to each other
 
+data_num_ = pd.DataFrame()
+for cat, val in CATEGORIES.items():
+    for v in val:
+        if v in data_num:
+            data_num_[v] = data_num[v]
+data_num_['k'] = data_num['k']
+data_num_['Efficiency'] = data_num['Efficiency']
+data_num = data_num_
 
-# data_num_ = pd.DataFrame()
-# for cat, val in CATEGORIES.items():
-#     for v in val:
-#         if v in data_num:
-#             data_num_[v] = data_num[v]
-# data_num_['k'] = data_num['k']
-# data_num_['Efficiency'] = data_num['Efficiency']
-# data_num = data_num_
+boxplot(data_num,
+        labels=[LABEL_MAP.get(label, label) for label in data_num.columns],
+        share_axes=False,
+        flierprops=dict(ms=2.0),
+        medianprops={"color": "black"},
+        fill_color='#01B0B9',
+        patch_artist=True,
+        show=False,
+        figsize=(7, 6),
+        )
+plt.subplots_adjust(wspace=0.05)
+plt.tight_layout()
+plt.show()
 
-# boxplot(data_num,
-#         labels=[LABEL_MAP.get(label, label) for label in data_num.columns],
-#         share_axes=False,
-#         flierprops=dict(ms=2.0),
-#         medianprops={"color": "black"},
-#         fill_color='#01B0B9',
-#         patch_artist=True,
-#         show=False,
-#         figsize=(7, 6),
-#         )
-# plt.subplots_adjust(wspace=0.05)
-# plt.tight_layout()
-# plt.show()
+# %%
+# Distribution of a feautre given a specific dye i.e.
+# Dist(Feature)|Dye
 
-# # %%
-# # Distribution of a feautre given a specific dye i.e.
-# # Dist(Feature)|Dye
+grps = data.groupby(by="Dye")
 
-# grps = data.groupby(by="Dye")
+f, axes = create_subplots(data_num.shape[1], figsize=(11, 8))
 
-# f, axes = create_subplots(data_num.shape[1], figsize=(11, 8))
+for col, ax in zip(data_num.columns, axes.flat):
 
-# for col, ax in zip(data_num.columns, axes.flat):
+    _, out = boxplot(
+    [grps.get_group('Indigo')[col].values, grps.get_group('Melachite Green')[col].values],
+        flierprops=dict(ms=2.0),
+        medianprops={"color": "black"},
+        fill_color=['#005066', '#B3331D'],
+        widths=0.7,
+        patch_artist=True,
+        ax=ax,
+        show=False
+    )
+    ax.set_xlabel(LABEL_MAP.get(col, col))
+    ax.set_xticks([])
+ax.legend([out["boxes"][0], out["boxes"][1]], ['Indigo', 'Melachite Green'],
+          loc=(-2.5, -1))
+plt.subplots_adjust(wspace=0.65, hspace=0.4)
+if SAVE:
+    plt.savefig("results/figures/boxplots.png", dpi=600, bbox_inches="tight")
+plt.tight_layout()
+plt.show()
 
-#     _, out = boxplot(
-#     [grps.get_group('Indigo')[col].values, grps.get_group('Melachite Green')[col].values],
-#         flierprops=dict(ms=2.0),
-#         medianprops={"color": "black"},
-#         fill_color=['#005066', '#B3331D'],
-#         widths=0.7,
-#         patch_artist=True,
-#         ax=ax,
-#         show=False
-#     )
-#     ax.set_xlabel(LABEL_MAP.get(col, col))
-#     ax.set_xticks([])
-# ax.legend([out["boxes"][0], out["boxes"][1]], ['Indigo', 'Melachite Green'],
-#           loc=(-2.5, -1))
-# plt.subplots_adjust(wspace=0.65, hspace=0.4)
-# if SAVE:
-#     plt.savefig("results/figures/boxplots.png", dpi=600, bbox_inches="tight")
-# plt.tight_layout()
-# plt.show()
+# %%
+# correlation of all input features with k
 
-# # %%
-# # correlation of all input features with k
+data_num1 = data_num.rename(columns=LABEL_MAP)
+data_num1.pop('Efficiency')
+plot_correlation(data_num1, show=False)
+if SAVE:
+    plt.savefig("results/figures/corr_all_k.png", dpi=600, bbox_inches="tight")
+plt.tight_layout()
+plt.show()
+# %%
+# correlation of all input features with Efficiency
 
-# data_num1 = data_num.rename(columns=LABEL_MAP)
-# data_num1.pop('Efficiency')
-# plot_correlation(data_num1, show=False)
-# if SAVE:
-#     plt.savefig("results/figures/corr_all_k.png", dpi=600, bbox_inches="tight")
-# plt.tight_layout()
-# plt.show()
-# # %%
-# # correlation of all input features with Efficiency
+data_num1 = data_num.rename(columns=LABEL_MAP)
+data_num1.pop('k')
+plot_correlation(data_num1)
 
-# data_num1 = data_num.rename(columns=LABEL_MAP)
-# data_num1.pop('k')
-# plot_correlation(data_num1)
+# %%
+# correlation of all input features with Efficiency and k
 
-# # %%
-# # correlation of all input features with Efficiency and k
+data_num1 = data_num.rename(columns=LABEL_MAP)
 
-# data_num1 = data_num.rename(columns=LABEL_MAP)
+plot_correlation(data_num1, show=False)
+if SAVE:
+    plt.savefig("results/figures/corr_all_k_e.png", dpi=600, bbox_inches="tight")
+plt.tight_layout()
+plt.show()
 
-# plot_correlation(data_num1, show=False)
-# if SAVE:
-#     plt.savefig("results/figures/corr_all_k_e.png", dpi=600, bbox_inches="tight")
-# plt.tight_layout()
-# plt.show()
+# %%
+# correlation of only input features which were termed as important
+# by Boruta method for k.
 
-# # %%
-# # correlation of only input features which were termed as important
-# # by Boruta method for k.
+cols = ['Solution pH', 'Cat. Loading (g/L)', 'O (At%)', 'Pore Size (nm)',
+        'HA (mg/L)', 'Mo (At%)', 'Light Int. (W)', #'Anions',
+        'Initial Conc. (mg/L)', 'Rxn Time (min)', 'Ni (At%)']
 
-# cols = ['Solution pH', 'Cat. Loading (g/L)', 'O (At%)', 'Pore Size (nm)',
-#         'HA (mg/L)', 'Mo (At%)', 'Light Int. (W)', #'Anions',
-#         'Initial Conc. (mg/L)', 'Rxn Time (min)', 'Ni (At%)']
+data_num1 = data_num.rename(columns=LABEL_MAP)[cols + ["k"]]
+plot_correlation(data_num1, annot_kws={"fontsize": 12}, show=False)
+if SAVE:
+    plt.savefig("results/figures/corr_k.png", dpi=600, bbox_inches="tight")
+plt.tight_layout()
+plt.show()
+# %%
+# plotting only those where correlation is higher than 0.6
+plot_correlation(data_num1, threshold=0.6, split="pos")
 
-# data_num1 = data_num.rename(columns=LABEL_MAP)[cols + ["k"]]
-# plot_correlation(data_num1, annot_kws={"fontsize": 12}, show=False)
-# if SAVE:
-#     plt.savefig("results/figures/corr_k.png", dpi=600, bbox_inches="tight")
-# plt.tight_layout()
-# plt.show()
-# # %%
-# # plotting only those where correlation is higher than 0.6
-# plot_correlation(data_num1, threshold=0.6, split="pos")
+# %%
+# plotting only those where correlation is below 0.5
+plot_correlation(data_num1, threshold=-0.5, split="neg")
 
-# # %%
-# # plotting only those where correlation is below 0.5
-# plot_correlation(data_num1, threshold=-0.5, split="neg")
+# %%
+# correlation of input features which were termed as important
+# by Boruta method.
 
-# # %%
-# # correlation of input features which were termed as important
-# # by Boruta method.
+data_num1 = data_num.rename(columns=LABEL_MAP)[cols + ["Efficiency"]]
+plot_correlation(data_num1, annot_kws={"fontsize": 12})
 
+# %%
 
+data_num1 = data_num.rename(columns=LABEL_MAP)[cols + ["k", "Efficiency"]]
+plot_correlation(data_num1, annot_kws={"fontsize": 12},
+                 show=False)
+if SAVE:
+    plt.savefig("results/figures/corr_selected.png", dpi=600, bbox_inches="tight")
+plt.tight_layout()
+plt.show()
+# %%
 
-# data_num1 = data_num.rename(columns=LABEL_MAP)[cols + ["Efficiency"]]
-# plot_correlation(data_num1, annot_kws={"fontsize": 12})
+mpl.rcParams.update(mpl.rcParamsDefault)
 
-# # %%
+data, encoders = prepare_data(outputs=["k", "Efficiency"])
+input_features = data.columns.tolist()
 
-# data_num1 = data_num.rename(columns=LABEL_MAP)[cols + ["k", "Efficiency"]]
-# plot_correlation(data_num1, annot_kws={"fontsize": 12},
-#                  show=False)
-# if SAVE:
-#     plt.savefig("results/figures/corr_selected.png", dpi=600, bbox_inches="tight")
-# plt.tight_layout()
-# plt.show()
-# # %%
+tsne = TSNE(random_state=313)
+comp = tsne.fit_transform(data[input_features])
 
-# mpl.rcParams.update(mpl.rcParamsDefault)
+def scatter_(first, second, label,
+             axs, fig):
 
-# data, encoders = prepare_data(outputs=["k", "Efficiency"])
-# input_features = data.columns.tolist()
+    c = data[col].values
 
-# tsne = TSNE(random_state=313)
-# comp = tsne.fit_transform(data[input_features])
+    pc = axs.scatter(first, second,
+                    c=c,
+                    s = 2,
+            cmap="Spectral",
+            )
 
-# def scatter_(first, second, label,
-#              axs, fig):
+    if label in ["Catalyst", "Anions", "Dye"]:
+        cmap = mpl.cm.Spectral
+        bounds = list(set(c))
+        norm = mpl.colors.BoundaryNorm(bounds + [bounds[-1]+1],
+                                       cmap.N)
 
-#     c = data[col].values
+        colorbar = fig.colorbar(
+            mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+            ticks = bounds,
+                 ax=axs, orientation='vertical')
 
-#     pc = axs.scatter(first, second,
-#                     c=c,
-#                     s = 2,
-#             cmap="Spectral",
-#             )
+        if label == "Catalyst":
+            ticklabels = range(18)
+        elif label == "Anions":
+            ticklabels = ['N/A', 'Cl', 'SO4', 'CO3', 'HCO3', 'HPO4']
+        elif label == "Dye":
+            ticklabels = ["MG", "INDIGO"]
+        else:
+            ticklabels = encoders[label].classes_
+        colorbar.ax.set_yticklabels(ticklabels)
 
-#     if label in ["Catalyst", "Anions", "Dye"]:
-#         cmap = mpl.cm.Spectral
-#         bounds = list(set(c))
-#         norm = mpl.colors.BoundaryNorm(bounds + [bounds[-1]+1],
-#                                        cmap.N)
+    else:
+        colorbar = fig.colorbar(pc, ax=axs)
+        label = LABEL_MAP.get(label, label)
+        colorbar.set_label(label)
 
-#         colorbar = fig.colorbar(
-#             mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
-#             ticks = bounds,
-#                  ax=axs, orientation='vertical')
+    despine_axes(colorbar.ax)
+    return
 
-#         if label == "Catalyst":
-#             ticklabels = range(18)
-#         elif label == "Anions":
-#             ticklabels = ['N/A', 'Cl', 'SO4', 'CO3', 'HCO3', 'HPO4']
-#         elif label == "Dye":
-#             ticklabels = ["MG", "INDIGO"]
-#         else:
-#             ticklabels = encoders[label].classes_
-#         colorbar.ax.set_yticklabels(ticklabels)
+# %%
 
-#     else:
-#         colorbar = fig.colorbar(pc, ax=axs)
-#         label = LABEL_MAP.get(label, label)
-#         colorbar.set_label(label)
+f, axes = create_subplots(29, sharex="all", sharey="all",
+                       figsize=(9, 9))
 
-#     despine_axes(colorbar.ax)
-#     return
+for col, ax in zip(input_features, axes.flat):
 
+    scatter_(comp[:, 0], comp[:, 1],
+         label=col, axs=ax, fig=f)
 
-# f, axes = create_subplots(29, sharex="all", sharey="all",
-#                        figsize=(9, 9))
-
-# for col, ax in zip(input_features, axes.flat):
-
-#     scatter_(comp[:, 0], comp[:, 1],
-#          label=col, axs=ax, fig=f)
-
-# plt.tight_layout()
-# plt.show()
+plt.tight_layout()
+plt.show()
